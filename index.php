560808b562f508ddb911d175512b41f3abb064c4
Pmnmpr <pmnmpr@gmail.com>
2021-09-22 11:56:42 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             614b19d985167             |
+    |_______________________________________|
+*/
+ use Pmpr\Package\Woocommerce\Woocommerce; Woocommerce::symcgieuakksimmu();
---
+++ src/Pmpr/Package/Woocommerce/Container.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             614b19d985167             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Package\Woocommerce; use Pmpr\Common\Foundation\Container\Container as BaseClass; class Container extends BaseClass { }
