819a703bd89d0606060c33f5de3570a99f273503
Pmnmpr <pmnmpr@gmail.com>
2023-03-01 18:22:30 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63ecdcefbbf12             |
+    |             63ff97bb54808             |
     |_______________________________________|
 */
  use Pmpr\Module\Woocommerce\Woocommerce; Woocommerce::symcgieuakksimmu();
--- src/Pmpr/Module/Woocommerce/Invoice/Test.php
+++ src/Pmpr/Module/Woocommerce/Invoice/Test.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             63ecdcefbbf12             |
+    |             63ff97bb54808             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Woocommerce\Invoice; class Test extends Common { }
