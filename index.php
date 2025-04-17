06c70425e2664d8b4a5c40d9de0236b8e1c23b43
pmpr.dev <pmpr.dev@gmail.com>
2025-04-17 13:48:05 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67d9d4f033e47             |
+    |             68010669d24e7             |
     |_______________________________________|
 */
  use Pmpr\Module\Woocommerce\Woocommerce; Woocommerce::symcgieuakksimmu();
--- src/Pmpr/Module/Woocommerce/Setting.php
+++ src/Pmpr/Module/Woocommerce/Setting.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             67d9d4f033e47             |
+    |             68010669d24e7             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Woocommerce; use Pmpr\Common\Cover\Woocommerce\AbstractSetting; use Pmpr\Common\Foundation\Interfaces\Constants; class Setting extends AbstractSetting { const qqiuqmkwamakmmky = self::soqkucakwksaeyce . 'variable_product_guide'; const kqaecmeyeicscaye = self::soqkucakwksaeyce . 'shop_table_view_columns'; public function qssqicawwgqqscui($ikgwqyuyckaewsow = []) { return $this->oaeygwkmgmgksqke([self::qqiuqmkwamakmmky => [Constants::qescuiwgsyuikume => __('Variation Product Guide', PR__MDL__WOOCOMMERCE)]], $ikgwqyuyckaewsow, self::imgaumeywmqsacas); } public static function ckgyyysykiycqwwm() { return self::iwgqamekocwaigci()->wikusamwomuogoau()->giiuwsmyumqwwiyq(self::qqiuqmkwamakmmky, ''); } }
